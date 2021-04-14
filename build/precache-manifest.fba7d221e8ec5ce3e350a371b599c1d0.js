0f7167481591c19d6f87379c27f0447939ba7a74
vietvulong <vietvulong@protonmail.com>
2021-04-14 22:11:01 +0700

    build folder

---
+++ build/precache-manifest.fba7d221e8ec5ce3e350a371b599c1d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7862c32ea328a978d8781e1e6138a38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0d3cc011f5bf0a07940",
+    "url": "/static/css/2.1dfb0863.chunk.css"
+  },
+  {
+    "revision": "49258a8b6745be89b4aa",
+    "url": "/static/css/main.8050216f.chunk.css"
+  },
+  {
+    "revision": "c0d3cc011f5bf0a07940",
+    "url": "/static/js/2.0b34a16f.chunk.js"
+  },
+  {
+    "revision": "c9039aa0c88234b9938181e263016f2a",
+    "url": "/static/js/2.0b34a16f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49258a8b6745be89b4aa",
+    "url": "/static/js/main.e407608b.chunk.js"
+  },
+  {
+    "revision": "487bdb5793803043d86e",
+    "url": "/static/js/runtime-main.80920847.js"
+  },
+  {
+    "revision": "cc802239cd92cca3fff755199a86e8f6",
+    "url": "/static/media/desert.cc802239.jpg"
+  }
+]);
