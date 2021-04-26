81d1e2be2301463364d36221586d1f013295d761
vietvulong <vietvulong@protonmail.com>
2021-04-26 09:39:49 +0700

    change endpoint

--- home/settings/base.py
+++ home/settings/base.py
@@ -64,6 +64,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SITE_ID = 1
 
