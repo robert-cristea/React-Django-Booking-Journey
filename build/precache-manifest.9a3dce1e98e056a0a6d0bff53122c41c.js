f7602ea9dd6602b72e854bd3f86f27eed7bb899a
vietvulong <vietvulong@protonmail.com>
2021-05-27 01:19:18 +0700

    make build folder

---
+++ build/precache-manifest.9a3dce1e98e056a0a6d0bff53122c41c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5d0eb614232bae29e6f184e1d8a6fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "808ebb1317708ed5c5e1",
+    "url": "/static/css/2.f353d5c0.chunk.css"
+  },
+  {
+    "revision": "a06599291b0e75bcc8e5",
+    "url": "/static/css/main.b7fe6b94.chunk.css"
+  },
+  {
+    "revision": "808ebb1317708ed5c5e1",
+    "url": "/static/js/2.0c9ccc38.chunk.js"
+  },
+  {
+    "revision": "a466d08e7758cef7ac3bf90573bb8ba2",
+    "url": "/static/js/2.0c9ccc38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a06599291b0e75bcc8e5",
+    "url": "/static/js/main.c5efaef3.chunk.js"
+  },
+  {
+    "revision": "487bdb5793803043d86e",
+    "url": "/static/js/runtime-main.80920847.js"
+  },
+  {
+    "revision": "cc802239cd92cca3fff755199a86e8f6",
+    "url": "/static/media/desert.cc802239.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
