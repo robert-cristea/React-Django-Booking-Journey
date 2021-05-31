df047a513f1b299512f571e09884133eff5f3b0d
vietvulong <vietvulong@protonmail.com>
2021-05-31 23:42:41 +0700

    select sort with dropdown

--- src/components/Search/SelectSort.js
+++ src/components/Search/SelectSort.js
@@ -1,54 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import Select, { NonceProvider } from 'react-select';
-
+import './SelectSort.css'
 const SelectSort = () => {
 
-  // const [selectedOption, setSelectedOption] = useState("");
-  const options = [
-    { value: 'match', label: 'Match' },
-    { value: 'price', label: 'Price' },
-  ];
-
-  const customStyles = {
-    option: (provided, state) => ({
-      ...provided,
-      //   borderBottom: '1px dotted pink',
-      color: state.isSelected ? 'white' : 'black',
-      paddingLeft: 15,
-    }),
-    control: (styles) => ({
-      ...styles,
-      width: 100,
-      border: 'none',
-      paddingRight: 10,
-      '&:hover': { borderColor: 'gray' }, // border style on hover
-      boxShadow: 'none', // no box-shadow
-    }),
-    singleValue: (provided, state) => {
-      const opacity = state.isDisabled ? 0.6 : 1;
-      const transition = 'opacity 500ms';
-
-      return { ...provided, opacity, transition };
-    }
-  }
-
+  const [selectedOption, setSelectedOption] = useState("Match");
   const handleChange = (value) => {
-    console.log(value);
+    setSelectedOption(value);
   };
 
   return (
     <div className="d-flex align-items-center px-3 py-2">
       <label htmlFor="category" className="pr-4 filter-label">SORT BY</label>
-      <Select
-        onChange={handleChange}
-        options={options}
-        styles={customStyles}
-        defaultValue={options[0]}
-        components={{
-          IndicatorSeparator: () => null
-        }}
-        isSearchable={false}
-      />
+      <div class="selectsort dropdown">
+        <button class="btn bg-white dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {selectedOption}<i class="fa fa-chevron-down ml-2" style={{fontSize:10, fontWeight:100}}></i>
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" onClick={() => handleChange('Match')} style={{borderBottom:"1px solid lightgray"}}>Match</a>
+          <a class="dropdown-item" onClick={() => handleChange('Price')}>Price</a>
+        </div>
+      </div>
     </div>
   )
 }
