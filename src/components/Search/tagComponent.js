dc159f3b2971a8bb01baefada1ec5103615f77e6
Vu Long <77659232+vietvulong@users.noreply.github.com>
2021-06-02 21:35:39 +0700

    Delete tagComponent.js

--- src/components/Search/tagComponent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TagComponent = (props) => {
-    return (
-        <div className="d-flex justify-content-center align-items-center px-2 py-1 my-1 mx-1" style={{ backgroundColor:"#00DAF8", borderRadius:"0.7rem",color:"white", fontSize:"0.8rem",height:"1.5rem" }}><span className="">{props.name}</span>{ props.case === "1"?<span className="pl-2" onClick={props.onClick} style={{cursor:"default"}}> &times;</span>:""}</div>
-    );
-}
-
-export default TagComponent;
